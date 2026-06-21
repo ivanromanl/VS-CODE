0760ba5dc4a6119c41ee925b1e3df1ff1a1340a1
ivan <iromanl@uees.edu.ec>
2026-06-20 21:23:46 -0700

    Agregar ejercicios basicos de Python

---
+++ Basic-math-operations.py
@@ -0,0 +1,9 @@
+sum_result =1 + 1
+subtraction_result = 90 - 430 
+multiplication_result =4 * 5
+division_result =50/10
+
+print("1+1=", sum_result)
+print("90-430=", subtraction_result)
+print("4*5=", multiplication_result)
+print("50/50=", division_result)
---
+++ Ejercicio-de Introduccion-a-python.py
@@ -0,0 +1,8 @@
+student_name ="Ivan"
+course_name = "Python Basic"
+favorite_language = "python"
+
+print("hello, welcome to my first python exercise.")
+print("my name is", student_name)
+print("I am learning", favorite_language)
+print("python is useful for creating programs.")
