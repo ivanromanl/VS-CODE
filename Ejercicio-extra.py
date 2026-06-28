83ee629294279368e403a17de51af950bfc7b111
ivan <iromanl@uees.edu.ec>
2026-06-28 13:12:16 -0700

    add Ejercicio extra

---
+++ Ejercicio-extra.py
@@ -0,0 +1,9 @@
+full_name = "Ivan Roman"
+age = "33"
+favorite_color = "Blue"
+favorite_food = "Encebollado"
+
+print(f"mi nombre es {full_name}")
+print(f"mi edad es {age}")
+print(f"mi color favorito {favorite_color}")
+print(f"mi comida favorita es el {favorite_food}")
