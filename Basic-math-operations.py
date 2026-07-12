5935b5c1786be9c5c5edb236cf4bc35b30e49608
ivan <iromanl@uees.edu.ec>
2026-07-12 13:26:04 -0700

    Update basic math operations

--- Basic-math-operations.py
+++ Basic-math-operations.py
@@ -1,9 +1,18 @@
-sum_result =1 + 1
-subtraction_result = 90 - 430 
-multiplication_result =4 * 5
-division_result =50/10
+"""Basic math operations."""
 
-print("1+1=", sum_result)
-print("90-430=", subtraction_result)
-print("4*5=", multiplication_result)
-print("50/50=", division_result)
+
+def main():
+    """Perform and display basic math operations."""
+    sum_result = 1 + 1
+    subtraction_result = 90 - 430
+    multiplication_result = 4 * 5
+    division_result = 50 / 10
+
+    print("1 + 1 =", sum_result)
+    print("90 - 430 =", subtraction_result)
+    print("4 * 5 =", multiplication_result)
+    print("50 / 10 =", division_result)
+
+
+if __name__ == "__main__":
+    main()
