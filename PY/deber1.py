077398a548b11d86ad1a3516e697d49f18115baa
ivan <iromanl@uees.edu.ec>
2026-08-23 09:19:27 -0700

    Update proyect

--- PY/deber1.py
+++ PY/deber1.py
@@ -1,22 +1,34 @@
-print("===CLASIFICACION DE PERSONAS SEGUN SU EDAD===")
+print("=== SUMAS ENTRE DISTINTOS TIPOS DE DATOS ===")
 
-nombre = input("ingrese su nombre:")
-apellido = input("ingrese su apellido:")
-edad = int(input("ingrese su edad:"))
+# 1 string + string
+print("\n1. string + string")
+print("Resultado:", "hola" + "mundo")
 
-if edad <0:
-    categoria = "edad no valida"
-elif edad <= 2:
-    categoria = "bebe"
-elif edad <= 9:
-    categoria = "nino"
-elif edad <= 12:
-    categoria = "preadolescente"
-elif edad <= 17:
-    categoria = "adolescente"
-elif edad <= 25:
-    categoria = "adulto joven"
-elif edad <= 59:
-    categoria = "adulto mayor"
+# 2. string + int
+print("\n2. string + int")
+try:
+    print("Resultados:" "Hola" + "mundo")
+except TypeError as error:
+    print("Error:", error)
 
-print(f"{nombre} {apellido} tiene {edad} anios y es {categoria}.")
+# 3. int + string
+print("\n3. int + string")
+try:
+    print("Resultado:", 18 + "años")
+except TypeError as error:
+    print("Error:", error)
+
+# 4. list + list
+print("\n4. list + list")
+print("Resultado:", [1, 2] + [3,4])
+
+# 5. string + list
+print("\n5. string + list")
+try:
+    print("Resultado:", "Numeros: " + [1,2,3])
+except TypeError as error:
+    print("Error:", error)
+
+# 6. float + int
+print("\n6. float + int")
+print("Resultado:", True + False)
