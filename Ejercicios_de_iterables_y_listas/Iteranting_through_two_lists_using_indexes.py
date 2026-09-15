7d5bfc563373e94a917f1205c37d8f78f8c88377
ivan <iromanl@uees.edu.ec>
2026-09-14 18:43:25 -0700

    update exercises

---
+++ Ejercicios_de_iterables_y_listas/Iteranting_through_two_lists_using_indexes.py
@@ -0,0 +1,6 @@
+# Exercise 1: Iterating Through Two lists using indexes
+first_list =["hay", "en", "interacion", "indice", "muy",]
+second_list =["casos", "los", "por", "es", "util"]
+
+for index in range(len(first_list)):
+    print(first_list[index], second_list[index])
